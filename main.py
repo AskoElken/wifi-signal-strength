3f6a5a71702e39b7882a3e863b89fd3fdc01adbe
Elken <asko.elken@artify.ee>
2023-09-23 14:56:38 +0300

    Minor fixes. List sorted by ssid and signal strength. Connected device is selected by ssid, channel and by addition signal strength (+-10) to get right line colored

--- main.py
+++ main.py
@@ -2,23 +2,29 @@ import subprocess
 import time
 import curses
 import re
+import math
 
-def get_connected_ssid_by_channel():
+def get_connected_device_info():
     try:
         # Run the airport command to get Wi-Fi info
         airport_output = subprocess.check_output(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I']).decode('utf-8')
 
-        # Use regular expressions to extract the connected channel
+        # Get connected channel
         channel_match = re.search(r'channel: (\d+)', airport_output)
 
-        if channel_match:
+        # Get connection signal strength
+        signal_strength_match = re.search(r'agrCtlRSSI: (-\d+)', airport_output)
+
+        # Get connected SSID
+        ssid_match = re.search(r'SSID: (.+)', airport_output)
+
+        if channel_match and signal_strength_match and ssid_match:
             connected_channel = channel_match.group(1)
-            # Run the airport command to get the SSID on the connected channel
-            airport_output = subprocess.check_output(['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-s', '-c', connected_channel, '-I']).decode('utf-8')
-            # Use regular expressions to extract the connected SSID
-            ssid_match = re.search(r'SSID: (.+)', airport_output)
-            if ssid_match:
-                return connected_channel, ssid_match.group(1)
+            connected_device_signal_strength = signal_strength_match.group(1)
+            connected_ssid = ssid_match.group(1)
+
+            return connected_channel, connected_ssid, connected_device_signal_strength
+
         return ''
     except subprocess.CalledProcessError as e:
         return ''
@@ -27,7 +33,7 @@ def get_wifi_signal_strength(stdscr):
     while True:
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        connected_channel, connected_ssid = get_connected_ssid_by_channel()
+        connected_channel, connected_ssid, connected_device_signal_strength = get_connected_device_info()
 
         try:
             # Run the airport command to get Wi-Fi info
@@ -50,7 +56,7 @@ def get_wifi_signal_strength(stdscr):
                     if channel != 'RSSI':
                         wifi_info.append((ssid, signal_strength, channel))
 
-            wifi_info.sort(key=lambda x: str(x[2]))  # Sort the list by channel
+            wifi_info.sort(key=lambda x: (x[0], x[1]))  # Sort the list by ssid and signal strength
 
             # Clear the screen
             stdscr.clear()
@@ -60,7 +66,11 @@ def get_wifi_signal_strength(stdscr):
             for ssid, signal_strength, channel in wifi_info:
                 display_text = "SSID: {:<20} Signal Strength: {:<10} Channel: {:<5}".format(ssid, signal_strength, channel)
 
-                if ssid == connected_ssid and channel == connected_channel:
+                if ssid == connected_ssid \
+                        and channel == connected_channel \
+                        and math.isclose(float(signal_strength), float(connected_device_signal_strength), abs_tol=10):
+                    # If a match then bold the connected device. A small hack to match signal strength because
+                    # those are measured a bit of different times and can vary in small amount. So, I wrote tolerance (+/- 10) in.
                     stdscr.addstr(row, 0, display_text, curses.color_pair(1))
                 else:
                     stdscr.addstr(row, 0, display_text)
